9dd8eb60dad0f7a16d2fe51bdc419ea9b46b25ac
Andrew Nhnagud <Andrewnhangud3785@gmail.com>
2025-05-15 15:37:55 +0200

    added imports and documents

--- challenge 2/challenge e/script.js
+++ challenge 2/challenge e/script.js
@@ -1,1 +1,9 @@
-console.log('Roles:', nwabisa.role, johanes.role, alex.role)
+import { display as nwabisa } from "./nwabisa.js";
+import { display as johannes } from "./johannes.js";
+import { display as alex } from "./alex.js";
+
+document.querySelector("#nwabisa").innerText = nwabisa;
+document.querySelector("#johannes").innerText = johannes;
+document.querySelector("#alex").innerText = alex;
+
+console.log("Roles:", nwabisa, johannes, alex);
