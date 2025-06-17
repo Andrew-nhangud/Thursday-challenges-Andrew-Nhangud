9fb8ec584a3f2293488ec11f59b6571c77af29c0
Andrew Nhnagud <Andrewnhangud3785@gmail.com>
2025-06-17 13:36:38 +0200

    updated syntax in challenge 4

--- Challenge 4/Challenge g/script.js
+++ Challenge 4/Challenge g/script.js
@@ -1,42 +1,39 @@
 const FREE_WARNING = "Free shipping only applies to single customer orders";
 const BANNED_WARNING =
-  "Unfortunately, we do not ship to your country of residence";
+  "Unfortunately we do not ship to your country of residence";
 const NONE_SELECTED = 0;
 
-const currentLocation = "RSA";
-let customers = 1;
-let shipping;
-let currency = "$";
+const customers = 1;
+const currentlocation = "RSA";
+let currency = null;
+let shipping = null;
 
-let shoes = 300 * 1;
-let toys = 100 * 5;
-let shirts = 150 * NONE_SELECTED;
-let batteries = 35 * 2;
-let pens = 5 * NONE_SELECTED;
-const total = shoes + batteries + pens + shirts + toys;
+const shoes = 300 * 1;
+const toys = 100 * 5;
+const shirts = 150 * NONE_SELECTED;
+const batteries = 35 * 2;
+const pens = 5 * NONE_SELECTED;
 
-if (location === "RSA") {
-  shipping = 400;
+if (currentlocation === "RSA") {
   currency = "R";
-} else if (location === "NAM") {
-  shipping = 600;
-  currency = "$";
+  shipping = 400;
 } else {
-  shipping = 800;
+  currency = "$";
+  shipping = location === "NAM" ? 600 : 800;
 }
 
-if (total > 1000) {
-  if (location === "NAM" && customers < 2) {
-    if (location === "RSA") {
-      shipping = 0;
-    }
-  }
-}
+const total = shoes + batteries + pens + shirts + toys;
+const isFreeLocal = currency === "R" && total > 1000;
+const isFreeInt = currency === "$" && total > 60;
+const isFree = isFreeLocal || isFreeInt;
+const isFreeWarning = isFree && customers !== 1;
+const totalWithShipping = total + shipping;
+const isBanned = location === "NK";
 
-if (shipping === 0 && customers !== 1) {
-  console.log(FREE_WARNING);
-}
+if (isFreeWarning) console.log(FREE_WARNING);
+if (isFree && !isFreeWarning) shipping = 0;
 
-location === "NK"
-  ? console.log(BANNED_WARNING)
-  : console.log("Price:", currency, total + shipping);
+const withShipping = total + shipping;
+console.log(
+  isBanned ? BANNED_WARNING : "Price: " + currency + totalWithShipping
+);
