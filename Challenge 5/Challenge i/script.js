cdb3072a93ff546f6be8e46dc389a08d4e7e90bc
Andrew Nhnagud <Andrewnhangud3785@gmail.com>
2025-07-11 09:07:42 +0200

    updated snytax errors in challenge 5 i

--- Challenge 5/Challenge i/script.js
+++ Challenge 5/Challenge i/script.js
@@ -1,23 +1,5 @@
 const rent = 400;
-const tax = '8%';
-const food = 51.7501;
-const salary = 800;
-const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
-
-// Only change below this line
-
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
-}
-	
-console.log(balance.toFixed(3))
-
-/* const rent = 400;
-const tax = '8%';
+const tax = "8%";
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
@@ -25,11 +7,14 @@ const hourOfDay = 0;
 const minuteOfDay = 0;
 
 // Only change below this line
+const isTime = hourOfDay === 0 && minuteOfDay === 0; // check to see if it is midnight aka 00:00
+letbalance = undefined;
 
-if (hourOfDay === 0 && minuteOfDay === 0) {
-    const taxAsDecimal = parseFloat(tax) / 100;
-    const startingAfterTax = salary - (salary * taxAsDecimal);
-    const balance = startingAfterTax - transport - food - rent;
+if (isTime) {
+  const taxAsDecimal = parseInt(tax) / 100;
+  const startingAfterTax = salary * (1 - taxAsDecimal);
+  const expenses = transport + food + rent;
+  balance = startingAfterTax - expenses;
+}
 
-    console.log(balance.toFixed(3)); // Prints rounded balance
-} */
+console.log(`R` + balance.toFixed(2));
